581ebea2bf1ed4d23dc6611c52386ba1312dd1c5
aadilraza339 <aadil18@navgurukul.org>
2020-02-19 11:52:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,19 +6,7 @@ var change_data = JSON.parse(data);
 
 var data = JSON.stringify(change_data)
 
-// console.log(data)
-
-// for (var i of change_data){
-//    if(i.exercises){
-//       var j = i.exercises;
-//       for (k of j){
-//          console.log(k)
-//       }
-//    }
-// }
-
 app.use(express.json());
-// console.log(change_data)
 
 app.get('/get',(req,res)=>{
    let data = fs.readFileSync(__dirname+'/new.json');
@@ -41,16 +29,13 @@ app.post("/post",(req,res)=>{
             exercises : []
          }
          change_data.push(courses)
-         // console.log(courses);
          fs.writeFile(__dirname+"/new.json",JSON.stringify(change_data,null,2))
             return res.send(courses)
       }
    })
 })
 
-// app.get('/get/:id',(req,res)=>{
-   
-// })
+
 app.put("/put/:id",(req,res)=>{
 
    fs.readFile(__dirname+"/new.json",(err,data)=>{
@@ -58,27 +43,6 @@ app.put("/put/:id",(req,res)=>{
          return res.send(err,"check you json file")
       }else{
          var change_data = JSON.parse(data);
-
-         // console.log(change_data);
-         // for(check of change_data){   
-         //    if(check.id == req.params.id){
-         //       var index = change_data.indexOf(check);
-         //       console.log(index);
-         //       var courses_PUT = {
-         //          name : req.body.name,
-         //          description : req.body.description,
-         //          id:check.id,
-         //          exercises:check.exercises
-         //       }
-         //    }
-           
-         // } // console.log(courses_PUT)
-         // var courses_PUT = {
-         //    name : req.body.name,
-         //    description : req.body.description,
-         //    id:req.params.id,
-         //    exercises:check.exercises
-         // }
          change_data[req.params.id-1].name =req.body.name;
          change_data[req.params.id-1].description = req.body.description;
          // console.log(change_data)  for check only for print
